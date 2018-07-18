2dad92b572fcf6c3b1c2f7b2ca7122187c935c53
harshakns <harshakns@gmail.com>
2018-07-18 11:40:39 +0530

    finally a working snake game

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let presentEvent='1';
 let previousEvent='';
 let pauseCount = 0;
 let idCreator = 0;
+
+let snakeState =[];
 //selecting the element for adding event listener
 let playField = document.getElementsByClassName('playField')[0];
 let overlay=document.getElementsByClassName('overlay')[0];
@@ -21,21 +23,29 @@ Head.style.top = window.getComputedStyle(Head, null).getPropertyValue('top');
 Food.style.left= window.getComputedStyle(Food, null).getPropertyValue('left');
 Food.style.top= window.getComputedStyle(Food, null).getPropertyValue('top');
 
+const changePlayFieldSpecs = (event) =>{
+    console.log(event.srcElement.innerWidth,event.srcElement.innerHeight);
+    let width = event.srcElement.innerWidth
+}
 
+// window.addEventListener('resize',changePlayFieldSpecs);
+const pressedKeyDown = (event) => { presentEvent = 'keydown'; onKeyPressed(event)}
+const releasedPressedKey = (event) => { presentEvent = 'keyup'; onKeyPressed(event)}
+const addEventListenerForInputs=()=>{
+    backgroundMain.addEventListener('keydown', pressedKeyDown);
+    backgroundMain.addEventListener('keyup', releasedPressedKey);
+}
+addEventListenerForInputs();
+const addEventListenerForRetryButton=()=>{
+    retryButton.addEventListener('click', () => overlayDeactivate());
 
-backgroundMain.addEventListener('keydown', event => {presentEvent='keydown';onKeyPressed(event)});
-backgroundMain.addEventListener('keyup', event => {presentEvent='keyup'; onKeyPressed(event)});// 
-retryButton.addEventListener('click',()=>overlayDeactivate());
-
-console.log(Head.style.top,Head.style.left);
-
-
-
+}
+addEventListenerForRetryButton();
 const onKeyPressed = (event) => {
     const eventKeyArray =['ArrowRight','ArrowLeft','ArrowUp','ArrowDown']
     event.preventDefault()
     if(eventKeyArray.includes(event.key)){
-        console.log(event);
+        // console.log(event);
         event.preventDefault();
         if(previousEvent!=presentEvent){
             if (event.key === 'ArrowRight') {
@@ -56,7 +66,7 @@ const onKeyPressed = (event) => {
             }
             previousEvent = presentEvent;
             // console.log(Head.style.left, Head.style.top, Food.style.left, Food.style.top)
-            console.log(presentEvent,previousEvent);
+            // console.log(presentEvent,previousEvent);
         }
     }
 }
@@ -78,17 +88,34 @@ const pauseP=() =>{
 const OnRightKeyPress = () => {
     direction = 'right';
     timerIndex = setInterval(() => {
-        console.log(Head.style.left,Head.style.top);
+        // console.log(Head.style.left,Head.style.top);
         snakeEatsFood(Head.style.left, Head.style.top, Food.style.left, Food.style.top);
-        console.log(Head.style.left, Head.style.top, Food.style.left, Food.style.top);
+        // console.log(Head.style.left, Head.style.top, Food.style.left, Food.style.top);
         if (sleft + 20 > 1220) {
             clearInterval(timerIndex);
-            Head.style.left = "1240px"; 
-            sleft = 1240;
+            removeListenersForInputs();
+            Head.style.left = "1220px"; 
+            sleft = 1220;
             overlay.setAttribute('style',`z-index:3`);
         } else {
             Head.style.left = sleft+20+"px";
-            sleft=sleft+20
+            sleft=sleft+20;
+            snakeState.unshift([Head.style.left, Head.style.top]);
+            for (let i = 1; i < idCreator + 1; i++) {
+                let identifier = "s" + i;
+                let sElement = document.getElementById(identifier);
+                sElement.style.left = snakeState[i][0]
+                sElement.style.top = snakeState[i][1]
+            }
+        snakeState.pop();
+        if(snakeState.length>2){
+            if (arrayFilterCompare([Head.style.left, Head.style.top], snakeState.slice(1))){
+                clearInterval(timerIndex);
+                console.log('snake hit itself');
+                removeListenersForInputs();
+                overlay.setAttribute('style','z-index:3')
+            }
+        }
         }
     }, 75);
 
@@ -99,12 +126,30 @@ const OnLeftKeyPress = () => {
         snakeEatsFood(Head.style.left, Head.style.top, Food.style.left,Food.style.top);
         if (sleft - 20 < 0) {
             clearInterval(timerIndex);
-            Head.style.left = "-20px"; 
-            sleft = -20;
+            removeListenersForInputs();
+            Head.style.left = "0px"; 
+            sleft = 0;
             overlay.setAttribute('style', `z-index:3`)
         } else {
             Head.style.left=sleft-20+"px";
             sleft=sleft - 20 ;
+            snakeState.unshift([Head.style.left, Head.style.top]);
+            for (let i = 1; i < idCreator + 1; i++) {
+                let identifier = "s" + i;
+                let sElement = document.getElementById(identifier);
+                sElement.style.left = snakeState[i][0]
+                sElement.style.top = snakeState[i][1]
+            }
+        snakeState.pop();
+        if (snakeState.length > 2)
+            if (arrayFilterCompare([Head.style.left, Head.style.top], snakeState.slice(1))) {
+                clearInterval(timerIndex);
+                console.log('snake hit itself');
+                removeListenersForInputs();
+                overlay.setAttribute('style', 'z-index:3')
+            }
+        console.log(snakeState);
+        
 
         }
     }, 75);
@@ -116,13 +161,31 @@ const OnUpKeyPress = () => {
         snakeEatsFood(Head.style.left, Head.style.top, Food.style.left, Food.style.top);
         if (stop - 20 < 0) {
             clearInterval(timerIndex);
+            removeListenersForInputs();
             Head.setAttribute('style', `top:-20`);
-            Head.style.top = "-20px";
-            stop = -20;
+            Head.style.top = "0px";
+            stop = 0;
             overlay.setAttribute('style', `z-index:3`)
         } else {
             Head.style.top = stop-20 +"px"
             stop = stop-20;
+            snakeState.unshift([Head.style.left, Head.style.top]);
+            for (let i = 1; i < idCreator + 1; i++) {
+                let identifier = "s" + i;
+                let sElement = document.getElementById(identifier);
+                sElement.style.left = snakeState[i][0]
+                sElement.style.top = snakeState[i][1]
+            }
+        snakeState.pop();
+        if (snakeState.length > 2)
+            if (arrayFilterCompare([Head.style.left, Head.style.top], snakeState.slice(1))) {
+                clearInterval(timerIndex);
+                console.log('snake hit itself');
+                removeListenersForInputs();
+                overlay.setAttribute('style', 'z-index:3')
+            }
+
+        console.log(snakeState);
         }
     }, 75);
 }
@@ -132,36 +195,46 @@ const OnDownKeyPress = () => {
         snakeEatsFood(Head.style.left, Head.style.top, Food.style.left, Food.style.top);
         if (stop + 20 > 480) {
             clearInterval(timerIndex);
-            Head.style.top="500px";
-            stop=500;
+            removeListenersForInputs();
+            Head.style.top="480px";
+            stop=480;
             overlay.setAttribute('style', `z-index:3`)
         } else {
             Head.style.top = stop+20 +"px";
             stop =stop + 20;
+            snakeState.unshift([Head.style.left, Head.style.top]);
+            for (let i = 1; i < idCreator + 1; i++) {
+                let identifier = "s" + i;
+                let sElement = document.getElementById(identifier);
+                sElement.style.left = snakeState[i][0]
+                sElement.style.top = snakeState[i][1]
+            }
+        snakeState.pop();
+        if (snakeState.length > 2)
+            if (arrayFilterCompare([Head.style.left, Head.style.top], snakeState.slice(1))) {
+                clearInterval(timerIndex);
+                console.log('snake hit itself');
+                removeListenersForInputs();
+                overlay.setAttribute('style', 'z-index:3')
+            }
+        console.log(snakeState);
         }
     }, 75);
 }
-
-
 const snakeEatsFood = (sl, st, fl, ft) => {
-    console.log(sl,st,fl,ft);
+    // console.log(sl,st,fl,ft);
         if (sl === fl && st === ft) {
-            console.log('snake ate the food');
+            // console.log('snake ate the food');
             Food.style.left = 20*getRandomInt(62)+ "px";
             Food.style.top = 20*getRandomInt(25) + "px";
-            console.log(sl,st,fl, ft);
-            createSnakeLinks();
+            // console.log(sl,st,fl, ft);
+            for(let i=0;i<5;i++){
+            createSnakeLinks();}
         }
 }
 const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 }
-const snakeDiesOnEdges = () => {
-    overlay.setAttribute('style', `z-index:3`);
-    clearInterval(timerIndex);
-
-}
-
 const overlayDeactivate = () => {
     console.log('overlay Deactivate initiated');
     clearInterval(timerIndex);
@@ -177,52 +250,69 @@ const overlayDeactivate = () => {
     if (idCreator>0){
         for(let i=1;i<idCreator+1;i++){
             let identifier = "s"+i;
-            sElement=document.getElementById(identifier);
+            let sElement=document.getElementById(identifier);
             console.log(i,'removed');
             sElement.parentNode.removeChild(sElement);
         }
     }
+    addEventListenerForInputs();
     idCreator=0;
     direction = '';
     presentEvent = '1';
     previousEvent = '';
-    offsetCount=true;
+    snakeState = [];
+    // offsetCount=true;
     overlay.setAttribute('style', `z-index:-2`);
-    console.log(Head.style.left, Head.style.top, Food.style.left, Food.style.top);
+    // console.log(Head.style.left, Head.style.top, Food.style.left, Food.style.top);
 }
-const createSnakeBody = () => {}
-//identify the end
-//join the new body beginning to the end.
-
-
 const createSnakeLinks =() => {
     let sBody = document.createElement('DIV');
-    sBody.style.top= "40px";// to be added
-    sBody.style.left= "20px";//to be added
+    sBody.style.top= Head.style.top;
+    sBody.style.left= Head.style.left;
     sBody.style.position= "absolute";
     sBody.style.zIndex="2";
     sBody.style.width= "20px";
     sBody.style.height= "20px";
-    sBody.style.border= "1px solid blue";
+    sBody.style.border= "2px solid blue";
     sBody.style.backgroundColor= "yellow";
     idCreator=idCreator + 1;
     sBody.id="s"+idCreator;
-    // sBody.className='sBody';
-    // sBody.id = "s"+idcreator;
-    // let connector = idcreator -1;
-    // idCreator = idCreator + 1;
-    // sBody.snakeConnector = connector;
-    // sBody.dataOwn = direction;
-    // sBody.dataRelay = direction;
-    // sBody.class = "snakeBody";
+
     playField.appendChild(sBody,null);
+    snakeState.push([sBody.style.left,sBody.style.top]);
+    console.log(idCreator)
     
 }
-const linkSnakeBody = (element) =>{
-    let predessor = document.getElementById("s"+connector);
-    if(predessor.dataRelay === element.dataOwn){
-        //do nothing
-    }else{
-        element.dataOwn = predessor.dataRelay
-        //set the changes
-    }}
+const removeListenersForInputs =( )=>{
+
+    backgroundMain.removeEventListener('keydown', pressedKeyDown);
+    backgroundMain.removeEventListener('keyup', releasedPressedKey);
+
+}
+const arrayCompare =(arr1,arr2)=>{
+    for(array of arr2){
+        if(arr1[0]===arr2[0]&&arr1[1]===arr2[1]){
+            return true;
+        }
+    }
+    
+
+}
+const arrayCompareMap =(arr1,arr2)=>{
+    arr2.map((e)=>{if(arr1[0]===e[0]&& arr1[1]===e[1]){return true} else{return false}})
+    return arr2;
+}
+const arrayCompareReduce =(arr)=>{
+    const reducer1 = (acc,cur)=> {acc+cur};
+    return arr.reduce(reducer);
+}
+
+const arrayFilterCompare=(arr1,arr2)=>{
+   if(arr2.filter(element=>arr1[0]===element[0]).filter(e=>arr1[1]===e[1]).length != 0){
+       return true;
+   }
+   else{
+       return false;
+   }
+}
+// arrayCompareMap([Head.style.left,Head.style.top],snakeState.slice(1))
